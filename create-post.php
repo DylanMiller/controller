c6186ee1bd0895c621ba6aef3c48edab291ed773
Dylan Miller <dmiller3199@gmail.com>
2014-12-04 09:14:39 -0800

    41

--- create-post.php
+++ create-post.php
@@ -6,12 +6,17 @@
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
     
+    $date = new DateTime('today');
+    $time = new DateTime('America/Los_Angeles');
+    
+    
     $query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
     echo "<p>Title: $title</p>";
     echo "<p>Post: $post</p>";
     
     if($query) {
         echo "<p>Successfully inserted post: </p>";
+        echo "Posted on: " . $date->format('M/D' . " " . 'd/Y') . " at " . $time->format("G:i");
     } 
     else {
          echo "<p>$connection->error</p>";
